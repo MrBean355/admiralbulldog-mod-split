591cd0012cce79903490e243d269b11a38f39e15
Mike Johnston <mrbean355@gmail.com>
2020-10-10 12:44:16 +0200

    Upload mods to S3 that don't exist yet

--- buildSrc/src/main/kotlin/PublishModTask.kt
+++ buildSrc/src/main/kotlin/PublishModTask.kt
@@ -5,12 +5,16 @@ import org.gradle.api.tasks.TaskAction
 import software.amazon.awssdk.core.sync.RequestBody
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request
 import software.amazon.awssdk.services.s3.model.ObjectCannedACL.PUBLIC_READ
 import software.amazon.awssdk.services.s3.model.PutObjectRequest
 
-private const val VPK_FILE = "compiled/pak01_dir.vpk"
+private const val VPK_FILE = "pak01_dir.vpk"
+private const val OUTPUT_VPK_FILE = "compiled/$VPK_FILE"
 private const val META_FILE = "meta.txt"
 private const val MODS_BUCKET = "dota-mods"
+
+private val s3 by lazy { S3Client.builder().region(Region.US_EAST_2).build() }
 
 /** Upload the mod's VPK and info to S3. */
 open class PublishModTask : DefaultTask() {
@@ -21,27 +25,26 @@ open class PublishModTask : DefaultTask() {
     fun run() {
         require(modName.isNotBlank()) { "Mod name must not be blank" }
         require(modDescription.isNotBlank()) { "Mod description must not be blank" }
-        val vpk = project.file(VPK_FILE)
+        val vpk = project.file(OUTPUT_VPK_FILE)
         require(vpk.exists()) { "VPK file does not exist" }
+        val modKey = project.name
 
-        if (!hasChanges()) {
+        if (!hasChanges(modKey) && RemoteMods.exists(modKey)) {
             return
         }
-
-        val s3 = S3Client.builder().region(Region.US_EAST_2).build()
 
         s3.putObject(
                 PutObjectRequest.builder()
                         .bucket(MODS_BUCKET)
                         .acl(PUBLIC_READ)
-                        .key("${project.name}/${vpk.name}")
+                        .key("$modKey/${vpk.name}")
                         .build(),
                 RequestBody.fromFile(vpk)
         )
         s3.putObject(
                 PutObjectRequest.builder()
                         .bucket(MODS_BUCKET)
-                        .key("${project.name}/$META_FILE")
+                        .key("$modKey/$META_FILE")
                         .build(),
                 RequestBody.fromString("""
                     name=$modName
@@ -50,7 +53,7 @@ open class PublishModTask : DefaultTask() {
         )
     }
 
-    private fun hasChanges(): Boolean {
+    private fun hasChanges(modKey: String): Boolean {
         val repo = FileRepositoryBuilder()
                 .findGitDir()
                 .build()
@@ -60,8 +63,25 @@ open class PublishModTask : DefaultTask() {
         }
 
         val git = Git.wrap(repo)
-        val status = git.status().addPath("${project.name}/compiled").call()
+        val status = git.status().addPath("$modKey/compiled").call()
 
         return !status.isClean
+    }
+}
+
+/** Remember which mods exist on S3. */
+private object RemoteMods {
+    private val mods: Set<String>
+
+    init {
+        val response = s3.listObjectsV2(ListObjectsV2Request.builder().bucket(MODS_BUCKET).build())
+        mods = response.contents().map { it.key() }
+                .filter { it.endsWith(VPK_FILE) }
+                .map { it.substringBefore('/') }
+                .toSet()
+    }
+
+    fun exists(name: String): Boolean {
+        return name in mods
     }
 }
